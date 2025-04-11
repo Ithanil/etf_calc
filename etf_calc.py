fb9320e68d803c8edd687c917c229c748852f362
Jan Kessler <jakessle@t-online.de>
2025-04-11 20:43:12 +0200

    add calculator tool and photo of the math

---
+++ etf_calc.py
@@ -0,0 +1,13 @@
+def calculate_etf_ratios(target_developed, developed_acwi, us_acwi, eu_acwi, gdpr_us_eu):
+    r_acwi = gdpr_us_eu * target_developed / (us_acwi + gdpr_us_eu*(developed_acwi - eu_acwi))
+    r_eu = target_developed - r_acwi * developed_acwi
+    return r_acwi, r_eu, (1. - r_acwi - r_eu)
+
+target_ratio = float(input("Enter desired developed/emerging market ratio (in %): ")) / 100.
+x, y, z = calculate_etf_ratios(target_ratio, 0.9, 0.65, 0.11, 1.5)
+if x < 0 or y < 0 or z < 0:
+    print("Error: One or more negative weights. Make sure your desired DM:EM ratio is achievable.")
+    exit(1)
+print(f"ACWI ETF: {x*100:.3f}%")
+print(f"EU ETF: {y*100:.3f}%")
+print(f"EM ETF: {z*100:.3f}%")
