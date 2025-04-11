4e7e2d03f257f9c98fb8f9a5800a90fcbc05151c
Jan Kessler <jakessle@t-online.de>
2025-04-11 22:26:48 +0200

    clarify some print statements

--- etf_calc.py
+++ etf_calc.py
@@ -3,10 +3,10 @@ def calculate_etf_ratios(target_developed, developed_acwi, us_acwi, eu_acwi, gdp
     r_eu = target_developed - r_acwi * developed_acwi
     return r_acwi, r_eu, (1. - r_acwi - r_eu)
 
-target_ratio = float(input("Enter desired developed/emerging market ratio (in %): ")) / 100.
+target_ratio = float(input("Enter desired developed market share (in %): ")) / 100.
 x, y, z = calculate_etf_ratios(target_ratio, 0.9, 0.65, 0.11, 1.5)
 if x < 0 or y < 0 or z < 0:
-    print("Error: One or more negative weights. Make sure your desired DM:EM ratio is achievable.")
+    print("Error: One or more negative weights. Make sure your desired DM share is achievable.")
     exit(1)
 print(f"ACWI ETF: {x*100:.3f}%")
 print(f"EU ETF: {y*100:.3f}%")
